15c75952e7d83977defa0689bf6e1f247114d375
flaviosuico <flaviosuico@gmail.com>
2025-02-15 15:27:51 -0300

    Treinamento CS50P

---
+++ hellp.py
@@ -0,0 +1,5 @@
+# Coleta o nome do usuário e imprime uma saudação formatando o nome do usuário com a primeira letra maiúscula e as demais minúsculas.
+name = input("What is your name? ").strip().title() 
+
+# Imprime a saudação
+print(f"hello, {name} !")
